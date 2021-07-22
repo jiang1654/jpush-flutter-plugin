1ad9dcf0bf4e89cd4f99bff6422d72ee1eec3a00
Jin <fenglangmeng@outlook.com>
2021-07-22 15:54:24 +0800

    优化

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Workspace\android\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Workspace\flutter\jpush-flutter-plugin\example"
+export "FLUTTER_ROOT=D:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\developer\app_workspace\jpush-flutter-plugin\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
